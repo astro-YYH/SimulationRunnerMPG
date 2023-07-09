c41b66b9f836f59df0a7de73987930092013ebbf
astro-YYH <yanhui.yang@email.ucr.edu>
2023-07-09 14:08:47 -0700

    make sim arguments

--- make_sub/make_gen_sub.py
+++ make_sub/make_gen_sub.py
@@ -1,9 +1,6 @@
 '''
 make submission files for submission file generator for simulations
-command: python make_gen_sub.py --json_file=matterLatin_11p_90x3.json
---box=256 --npart=128 --nproc=256 --cores=32 --py_script=make_sim_sub.py
---submit_base=gen --gadget_dir=~/bigdata/codes/MP-Gadget/
---cluster_class=clusters.BIOClass --outdir_base=~/bigdata/test_sims/cosmo_11p
+command: python make_gen_sub.py --json_file=../latin_design/matterLatin_11p_90x3.json --box=100 --npart=75 --nproc=256 --cores=32 --py_script=make_sim_sub.py --gadget_dir=~/bigdata/codes/MP-Gadget3/ --cluster_class=clusters.BIOClass --outdir_base=/rhome/yyang440/bigdata/tentative_sim_suite/L1/cosmo_11p
 '''
 from typing import Generator
 import argparse
@@ -31,15 +28,16 @@ def take_params_dict(Latin_dict: dict) -> Generator:
 
 def write_gen_submit(index: int, box: int,   npart: int,
         hubble:     float, omega0: float, omegab: float,
-        scalar_amp: float, ns:     float,
+        scalar_amp: float, ns:     float, w0:     float,
+        wa:         float, mnu:    float, Neff:   float,
+        alphas:     float, MWDM:   float,
         nproc :     int,   cores: int,
         outdir:     str = "data",
-        submit_base: str = "gen",
         gadget_dir: str = "~/bigdata/MP-Gadget3/",
         python:     str = "python", # it's annoying
         py_script:  str = "make_sim_sub.py",
         cluster_class: str = "clusters.BIOClass"):
-    with open(submit_base + "_{}.submit".format(str(index).zfill(4)), "w") as f:
+    with open("gen_Box{}_Part{}_{}.submit".format(box, npart, str(index).zfill(4)), "w") as f:
         f.write("#!/bin/bash\n")
         f.write("#SBATCH --partition=short\n")    
         f.write("#SBATCH --job-name={}\n".format(outdir[-6:]))   
@@ -49,12 +47,12 @@ def write_gen_submit(index: int, box: int,   npart: int,
         f.write("#SBATCH --cpus-per-task=1\n")
         f.write("#SBATCH --mem=6G\n")
         f.write("# SBATCH --mail-type=end\n")
-        f.write("# SBATCH --mail-user=yyang440@ucr.edu\n\n")
-        f.write("#SBATCH --exclude=c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39,c40,c41,c42,c43,c44,c45,c46,c47,c48\n")
+        f.write("# SBATCH --mail-user=yyang440@ucr.edu\n")
+        f.write("#SBATCH --exclude=c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39,c40,c41,c42,c43,c44,c45,c46,c47,c48\n\n")
         f.write("which python\n")
         f.write("current_datetime=$(date \"+%Y-%m-%d %H:%M:%S\")\n")
         f.write("echo \"Current date and time: $current_datetime\"\n")
-        f.write("python {} --box={} --npart={} --hubble={} --omega0={} --omegab={} --scalar_amp={} --ns={} --nproc={} --cores={} --outdir={} --gadget_dir={} --python={} --cluster_class={}\n".format(py_script, str(box), str(npart), str(hubble), str(omega0), str(omegab),str(scalar_amp),str(ns),str(nproc),str(cores), outdir, gadget_dir, python, cluster_class))
+        f.write("python {} --box={} --npart={} --hubble={} --omega0={} --omegab={} --scalar_amp={} --ns={} --w0={} --wa={} --mnu={} --Neff={} --alphas={} --MWDM={} --nproc={} --cores={} --outdir={} --gadget_dir={} --python={} --cluster_class={}\n".format(py_script, str(box), str(npart), str(hubble), str(omega0), str(omegab),str(scalar_amp),str(ns), str(w0), str(wa), str(mnu), str(Neff), str(alphas), str(MWDM), str(nproc),str(cores), outdir, gadget_dir, python, cluster_class))
         f.write("current_datetime=$(date \"+%Y-%m-%d %H:%M:%S\")\n")
         f.write("echo \"Current date and time: $current_datetime\"\n")           
 
--- make_sub/make_sim_sub.py
+++ make_sub/make_sim_sub.py
@@ -28,6 +28,14 @@ if __name__ == "__main__":
     parser.add_argument("--scalar_amp", type=float, default=2.427e-9)
     parser.add_argument("--ns", type=float, default=.97)
     parser.add_argument("--hubble", type=float, default=.7)
+    # extensions
+    parser.add_argument("--w0", type=float, default=-1.)
+    parser.add_argument("--wa", type=float, default=0.)
+    parser.add_argument("--mnu", type=float, default=0.)
+    parser.add_argument("--Neff", type=float, default=3.046)
+    parser.add_argument("--alphas", type=float, default=0.)
+    parser.add_argument("--MWDM", type=float, default=0.)
+
 
     parser.add_argument("--python", type=str, default="python")
 
@@ -51,12 +59,12 @@ ns = args.ns # Scalar spectral index
 nproc = args.nproc # Total number of processors
 cores = args.cores # Number of cores per node
 
-m_nu = .1 # total neutrino mass
-w0_fld = -1.1
-wa_fld = .2
-alpha_s = .1 # Running of the spectral index
-N_ur = 3.5
-MWDM_therm = 1.
+m_nu = mnu # total neutrino mass
+w0_fld = w0
+wa_fld = wa
+alpha_s = alphas # Running of the spectral index
+N_ur = Neff
+MWDM_therm = MWDM
 
 # set the paths
 outdir = args.outdir # Output folder name
