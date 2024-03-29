b3c434293755b8f5319821ecd27c917f83abc7ee
Yanhui Yang <yanhui.yang@email.ucr.edu>
2024-03-28 20:51:10 -0700

    new SLHD

--- latin_design/SLHD_StandDesign.R
+++ latin_design/SLHD_StandDesign.R
@@ -3,8 +3,8 @@ library(SLHD)
 
 set.seed(1)
 
-t <- 280 # number of slices
-m <- 4 # number of samples per slice
+t <- 4 # number of slices
+m <- 3 # number of samples per slice
 k <- 11 # number of parameters per sample
 #Maximin-distance sliced Latin hypercube designs
 D2<-maximinSLHD(t = t, m = m, k = k) 
